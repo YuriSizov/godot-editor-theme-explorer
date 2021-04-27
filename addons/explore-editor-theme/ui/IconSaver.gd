a0f7492b5adcd7c39670601c594c65d5b1625fa5
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2021-04-27 19:33:50 +0200

    Make node reference naming consistent

--- addons/explore-editor-theme/ui/IconSaver.gd
+++ addons/explore-editor-theme/ui/IconSaver.gd
@@ -7,35 +7,35 @@ signal filesystem_changed()
 export var icon_name: String = ""
 export var type_name: String = ""
 
-onready var _save_button: Button = $SaveButton
-onready var _save_dialog: FileDialog = $SaveDialog
-onready var _message: Label = $Message
+onready var save_button: Button = $SaveButton
+onready var save_dialog: FileDialog = $SaveDialog
+onready var message: Label = $Message
 
 
 func _ready() -> void:
 	connect("visibility_changed", self, "_on_visibility_changed")
-	_save_button.connect("pressed", self, "_on_save_button_pressed")
-	_save_dialog.connect("file_selected", self, "_on_file_selected")
+	save_button.connect("pressed", self, "_on_save_button_pressed")
+	save_dialog.connect("file_selected", self, "_on_file_selected")
 
 
 func _on_visibility_changed() -> void:
-	_message.text = ""
+	message.text = ""
 
 
 func _on_save_button_pressed() -> void:
-	_save_dialog.popup_centered()
+	save_dialog.popup_centered()
 
 
 func _on_file_selected(path: String) -> void:
 	
 	if not has_icon(icon_name, type_name):
-		_message.text = "Can't load icon. Is a valid icon selected?"
+		message.text = "Can't load icon. Is a valid icon selected?"
 		return
 	
 	var unique_icon := get_icon(icon_name, type_name).duplicate(true)
 	if ResourceSaver.save(path, unique_icon) != OK:
-		_message.text = "Error while saving icon."
+		message.text = "Error while saving icon."
 		return
 	
-	_message.text = "Successfully saved as '%s'." % path
+	message.text = "Successfully saved as '%s'." % path
 	emit_signal("filesystem_changed")
