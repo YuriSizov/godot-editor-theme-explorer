deebeaf4785267ce21a18ab55fb1a8797e08390d
Yuri Sizov <yuris@humnom.net>
2021-05-27 18:19:05 +0300

    Improve feedback from action buttons

--- addons/explore-editor-theme/ui/CodePreview.gd
+++ addons/explore-editor-theme/ui/CodePreview.gd
@@ -11,12 +11,16 @@ var code_text : String = "" setget set_code_text
 func _ready() -> void:
 	copy_code_button.connect("pressed", self, "_on_copy_button_pressed")
 
+# Properties
 func set_code_text(value : String) -> void:
 	code_input.text = value
+	copy_code_button.icon = null
 
+# Handlers
 func _on_copy_button_pressed() -> void:
 	var copied_text = code_input.text.strip_edges()
 	if (copied_text.empty()):
 		return
 	
 	OS.clipboard = copied_text
+	copy_code_button.icon = get_icon("StatusSuccess", "EditorIcons")
--- addons/explore-editor-theme/ui/IconSaver.gd
+++ addons/explore-editor-theme/ui/IconSaver.gd
@@ -1,41 +1,75 @@
 tool
 extends VBoxContainer
 
-
 signal filesystem_changed()
 
-export var icon_name: String = ""
-export var type_name: String = ""
+# Public properties
+var icon_name: String = "" setget set_icon_name
+var type_name: String = "" setget set_type_name
 
+# Node references
 onready var save_button: Button = $SaveButton
 onready var save_dialog: FileDialog = $SaveDialog
-onready var message: Label = $Message
+onready var status_message: Label = $Message
 
-
-func _ready() -> void:
-	connect("visibility_changed", self, "_on_visibility_changed")
+func _ready() -> void:	
+	connect("visibility_changed", self, "_clear_status")
 	save_button.connect("pressed", self, "_on_save_button_pressed")
 	save_dialog.connect("file_selected", self, "_on_file_selected")
 
+# Properties
+func set_icon_name(value : String) -> void:
+	icon_name = value
+	_clear_status()
 
-func _on_visibility_changed() -> void:
-	message.text = ""
+func set_type_name(value : String) -> void:
+	type_name = value
+	_clear_status()
 
+# Helpers
+func _show_status(success : bool, message : String = "") -> void:
+	if (!is_inside_tree() || !Engine.editor_hint):
+		return
+	
+	if (success):
+		save_button.icon = get_icon("StatusSuccess", "EditorIcons")
+	else:
+		save_button.icon = get_icon("StatusError", "EditorIcons")
+	
+	if (message):
+		status_message.text = message
+		if (success):
+			status_message.add_color_override("font_color", get_color("font_color", "Label"))
+		else:
+			status_message.add_color_override("font_color", get_color("error_color", "Editor"))
+		
+		status_message.show()
+	else:
+		status_message.hide()
 
+func _clear_status() -> void:
+	if (!is_inside_tree() || !Engine.editor_hint):
+		return
+	
+	save_button.icon = null
+	status_message.hide()
+
+# Handlers
 func _on_save_button_pressed() -> void:
+	if (icon_name):
+		save_dialog.current_file = "%s.png" % icon_name
 	save_dialog.popup_centered()
 
-
 func _on_file_selected(path: String) -> void:
-	
-	if not has_icon(icon_name, type_name):
-		message.text = "Can't load icon. Is a valid icon selected?"
+	if (!has_icon(icon_name, type_name)):
+		_show_status(false, "Can't load icon. Is a valid icon selected?")
 		return
 	
 	var unique_icon := get_icon(icon_name, type_name).duplicate(true)
-	if ResourceSaver.save(path, unique_icon) != OK:
-		message.text = "Error while saving icon."
+	var error = ResourceSaver.save(path, unique_icon)
+	if (error != OK):
+		_show_status(false, "Error while saving icon.")
 		return
 	
-	message.text = "Successfully saved as '%s'." % path
+	_show_status(true)
 	emit_signal("filesystem_changed")
